63f768756a078d21d9d103e6840d370d7e02b72f
Ryan Hopper-Lowe <rhopperlowe@gmail.com>
2023-03-16 00:01:02 -0500

    implement listing detail page

--- src/app/listing-detail/listing-detail.component.ts
+++ src/app/listing-detail/listing-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listing-detail',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./listing-detail.component.scss']
 })
 export class ListingDetailComponent {
+  listing?: Listing;
 
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.listing = fakeListings.find((item) => item.id === id);
+  }
 }
