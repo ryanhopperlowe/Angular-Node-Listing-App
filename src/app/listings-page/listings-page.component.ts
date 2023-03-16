78fc8d76bfcda18c0a89960246a76a71eea9a7b4
Ryan Hopper-Lowe <rhopperlowe@gmail.com>
2023-03-16 00:00:15 -0500

    Display fake data on listings page

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { fakeListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-listings-page',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./listings-page.component.scss']
 })
 export class ListingsPageComponent {
+  listings: Listing[] = [];
 
+  ngOnInit(): void {
+    this.listings = fakeListings;
+  }
 }
